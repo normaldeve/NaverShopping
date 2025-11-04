013d256929f8772d8a1ec8278f79237cf9cebeac
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-04 12:47:29 +0900

    feat: WebClient 비동기 적용

--- src/main/java/com/navershop/navershop/config/WebClientConfig.java
+++ src/main/java/com/navershop/navershop/config/WebClientConfig.java
@@ -1,9 +1,16 @@
 package com.navershop.navershop.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
